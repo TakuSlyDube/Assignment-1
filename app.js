d5a2e577660150bef4fc228c0e369a0810b308c4
Takunda Sylvester Dube <Takunda.Dube@omnicontact.biz>
2025-09-05 10:54:43 +0200

    Update on readme file and rate limiting

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const rateLimit = require('express-rate-limit');
 require('dotenv').config();
 const fixtureRouter = require('./routes/fixtureRouter');
 
@@ -17,6 +18,12 @@ app.use((error, req, res, next) => {
   const data = error.data;
   res.status(status).json({ message, data });
 });
+
+let limiter = rateLimit({
+  max:1000,
+  windowMS: 60 * 60 * 1000,
+  message: " too many request from this IP. Please try again later."
+})
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
